816b51df16bc246ca7e35f87cadd31fb316564cb
Abbeblattelito <abdulaahi@hotmail.se>
2018-09-05 13:08:52 +0200

    uppgift 1

--- src/test_1.java
+++ src/test_1.java
@@ -1,4 +1,12 @@
 
 public class test_1 {
 
+	public static void main(String[] args) {
+	 int[] array = {1, 5, 5, 6 ,12};
+	 int sum = 0;
+	 for(int i : array) {
+		 sum += i;
+	 }
+	 System.out.println(sum);
+}
 }
